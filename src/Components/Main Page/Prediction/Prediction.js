96875c72283aefae8b39ac24504bc75f792d7c40
ajayaj0302 <ajayaj0302@gmail.com>
2022-08-03 10:10:25 +0530

    Prediction components started

--- src/Components/Main Page/Prediction/Prediction.js
+++ src/Components/Main Page/Prediction/Prediction.js
@@ -29,39 +29,37 @@ function Prediction() {
         </div>
 
         <Row>
-          <div className="my-4">
+          <Col sm={2} className="d-flex">
             <div className="d-flex align-items-center">
-              <div className="d-flex">
-                <div className="d-flex align-items-center">
-                  <img src={Bitcoin} alt="Bitcoin" />
-                  <p className="my-0 ms-3">Bitcoin</p>
-                </div>
-
-                <div
-                  className="mx-3"
-                  style={{
-                    height: "48x",
-                    borderLeft: "1px solid #B7C3CE",
-                  }}
-                ></div>
-
-                <div className="d-flex align-items-center">
-                  <img src={Tether} alt="Tether" />
-                  <p className="my-0 ms-3">Tether</p>
-                </div>
-              </div>
-
-              <div className="d-flex justify-content-between align-items-center">
-                <p className="text-primary fw-bold my-0">BTCUSDT</p>
-                <button
-                  type="button"
-                  className="text-white bg-primary text-decoration-none border border-0 px-3 py-2"
-                >
-                  <img src={Trade} alt="Trade" className="p-1 bg-white" /> Trade
-                </button>
-              </div>
+              <img src={Bitcoin} alt="Bitcoin" />
+              <p className="my-0 ms-3">Bitcoin</p>
             </div>
-          </div>
+
+            <div
+              className="mx-3"
+              style={{
+                height: "48x",
+                borderLeft: "1px solid #B7C3CE",
+              }}
+            ></div>
+
+            <div className="d-flex align-items-center">
+              <img src={Tether} alt="Tether" />
+              <p className="my-0 ms-3">Tether</p>
+            </div>
+          </Col>
+
+          <Col sm={10}>
+            <div className="d-flex justify-content-between align-items-center">
+              <p className="text-primary fw-bold my-0">BTCUSDT</p>
+              <button
+                type="button"
+                className="text-white bg-primary text-decoration-none border border-0 px-3 py-2"
+              >
+                <img src={Trade} alt="Trade" className="p-1 bg-white" /> Trade
+              </button>
+            </div>
+          </Col>
         </Row>
       </Container>
     </section>
